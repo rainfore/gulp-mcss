18bc2ea89802943b68fbb4292e2f7a98aea6dbc0
Capasky <capasky@live.com>
2015-12-24 13:03:13 +0800

    support multifile

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var through = require('through2');
 var gutil = require('gulp-util');
 var mcss = require('mcss');
+var replaceExt = require('replace-ext');
 
 var PluginError = gutil.PluginError;
 
-var extend = function(o1, o2, override) {
+var extend = function (o1, o2, override) {
     o1 = o1 || {};
     if (o2) {
         for (var i in o2) {
@@ -17,23 +18,25 @@ var extend = function(o1, o2, override) {
 }
 
 module.exports = function (opt) {
-    return through.obj(function(file, enc, cb) {
-        if (file.isNull()) return cb(null, file); 
+    return through.obj(function (file, enc, cb) {
+        if (file.isNull()) return cb(null, file);
         if (file.isStream()) return cb(new PluginError('gulp-mcss', 'Streaming not supported'));
 
         var options = extend(opt, {
             filename: file.path
         });
-
+        var self = this;
         try {
-            var data = mcss(options).translate().done(function(text) {
+            var data = mcss(options).translate().done(function (text) {
                 file.contents = new Buffer(text);
-                file.path = file.path.replace(/\.mcss$/, '.css');
-                cb(null, file);
-            }).fail(function(err){
+                file.path = replaceExt(file.path, '.css');
+                self.push(file);
+                cb();
+            }).fail(function (err) {
                 mcss.error.format(err);
                 console.log(err.message);
-                cb(null, file);
+                self.push(file);
+                cb();
             });
         } catch (err) {
             return cb(new PluginError('gulp-mcss', err));
